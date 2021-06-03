e99311fb4a663b4d5d0ad44bc9cfb71c413bccc4
sajzz <sajalspatil2704@gmail.com>
2021-06-03 14:46:17 +0530

    added func header

--- math.py
+++ math.py
@@ -1,12 +1,15 @@
+#add implementation
 def add(x,y):
     pass 
-    
+
+#sub implementation    
 def subtract(x,y):
     pass
-    
+ 
+#mul implementation 
 def multiply(x,y):
     pass
-    
+
+#div implementation   
 def divide(x,y):
     pass
- 
